7fc86fab5206419684759a04d714d1b141b31fcf
DarianPaulino <dpaulino@nyc.yearup.org>
2022-11-02 23:26:33 -0400

    firstCommit

---
+++ hello.js
@@ -0,0 +1,20 @@
+// Hello World
+
+"use strict"
+var message= "Hello World";
+console.log(message);
+
+
+var num1 = 15, num2 = 10;
+var answer;
+answer = num1 + num2;
+answer = num1 - num2; 
+answer = num1 * num2;
+answer = num1 / num2; 
+
+answer = num1 % num2; 
+console.log("1:" + answer);
+console.log("2: "+ answer);
+console.log("3:" + answer);
+console.log("4: " + answer);
+
